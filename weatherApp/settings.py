9d1f9a694c76ab0f1ee92417329cd7e80d12117f
Moe <moeyazdii@proton.me>
2025-04-08 17:25:51 +0330

    main page build

--- weatherApp/settings.py
+++ weatherApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weatherWeb'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'weatherApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'weatherApp'/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
